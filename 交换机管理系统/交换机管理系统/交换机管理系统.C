0671a8598ff8fbf119ef767168de68c017a66c14
suyaofan <549292816@qq.com>
2019-07-04 16:29:48 +0800

    交换机管理系统

---
+++ 交换机管理系统/交换机管理系统/交换机管理系统.C
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<windows.h>
+
+int main(void) {
+
+	printf("---交换机后台管理系统---\n");//打印控制台输出文字
+	printf("     1.创建账号\n");
+	printf("     2.删除账号\n");
+	printf("     3.端口管理\n");
+	printf("     4.网络测试\n");
+	printf("     5.VLAN管理\n");
+	printf("     6.退出\n");
+
+
+
+
+
+	system("pause");
+	return 0;
+}
